fa7d6cc6ba0aa14232701ea8738dc0aa7702c4e4
Mohamed Suhaib <104195241+suhaibahmed-01@users.noreply.github.com>
2022-07-23 17:44:07 +0530

    Create mark-x.py
    a program that allows you to mark a square on the map using a two-digit system. The first digit is the vertical column number and the second digit is the horizontal row number

---
+++ mark-x.py
@@ -0,0 +1,15 @@
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = int(input("Where do you want to mark x in thr grid? "))
+
+horizontal_position = (position[0])
+vertical_position = (position[1])
+
+map[vertical - 1] ="x"
+map[horizontal - 1] = "x"
+
+print(f"{row1}\n{row2}\n{row3}")
