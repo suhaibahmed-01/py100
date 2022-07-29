56449157fdb8663ac903250f3917accfcafeb8c6
Mohamed Suhaib <104195241+suhaibahmed-01@users.noreply.github.com>
2022-07-30 03:46:04 +0530

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,8 +1,8 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-method = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+method = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("\n\nType your message:\n").lower()
+shift = int(input("\n\nType the shift number:\n"))
  
 
 def caesar_cipher(input_text, shift_number, cipher_method):
@@ -13,7 +13,7 @@ def caesar_cipher(input_text, shift_number, cipher_method):
     index = alphabet.index(letter)
     new_index = index + shift_number
     output_text += alphabet[new_index]
-  print(f"Your {method}d output message is : {output_text}")
+  print(f"\n\nYour {method}d output message is : {output_text}")
 
 
 caesar_cipher(input_text = text, shift_number = shift, cipher_method = method)
